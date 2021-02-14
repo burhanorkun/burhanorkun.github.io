94f015f63fc318d93adb6c5a6bf6a1809b752c39
burhan orkun <beyazmartilar@gmail.com>
2021-02-14 14:11:58 +0300

    added gatsby

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   url: 'https://lumen.netlify.com',
-  pathPrefix: '/blog',
+  pathPrefix: '/',
   title: 'Blog by Burhan Orkun',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
