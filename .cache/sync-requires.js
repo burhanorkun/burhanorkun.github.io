83358a27a45320c29ecae2f07c2dfd101226b081
burhan orkun <beyazmartilar@gmail.com>
2021-02-14 19:52:47 +0300

    chanrged

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-templates-categories-list-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/categories-list-template.js"))),
+  "component---src-templates-category-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/category-template.js"))),
+  "component---src-templates-index-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/index-template.js"))),
+  "component---src-templates-not-found-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/not-found-template.js"))),
+  "component---src-templates-page-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/page-template.js"))),
+  "component---src-templates-post-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/post-template.js"))),
+  "component---src-templates-tag-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/tag-template.js"))),
+  "component---src-templates-tags-list-template-js": (preferDefault(require("/Users/burhanorkun/dev/my_personal_page/gatsby-starter-lumen/src/templates/tags-list-template.js")))
+}
+
